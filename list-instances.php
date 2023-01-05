e52c26e44f52f971a7a7c950253680ba0e29f578
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2023-01-05 05:15:51 +0100

    CHANGES: ADDED script to write ini script and first test of executing shell script to create sub domain

--- list-instances.php
+++ list-instances.php
@@ -11,7 +11,7 @@
  * @package           List_instances
  *
  * @wordpress-plugin
- * Plugin Name:       rpi Pinnwand
+ * Plugin Name:       rpi List Moodle Instanzen
  * Plugin URI:        https://github.com/rpi-virtuell/rpi-wall/
  * Description:       Wordpress Plugin used to display Moodle instances of the Server
  * Version:           1.1.2
@@ -29,16 +29,89 @@ class ListInstances
     {
         add_action('wp', [$this, 'sync_instances_with_ini']);
     }
-    public function sync_instances_with_ini(){
 
-        if (get_post_type() === 'instance' && is_archive())
-        {
-            var_dump(parse_ini_file(__DIR__.'/instances.ini',true));
-        }
-        if (false){
+    public function sync_instances_with_ini()
+    {
 
+        $ini_content = parse_ini_file(dirname(get_home_path()) . '/multi-moodle-instances/instances.ini', true);
+
+
+        $assoc_arr = [
+            'exclude_plugins' => [
+                'ex' => ['test' => 'sso']
+            ]
+            ,
+            'domain' => ['main_host_domain' => '.gpendialogue.net']
+
+        ];
+
+        $instances = get_posts([
+            'numberposts' => -1,
+            'post_type' => 'instance'
+        ]);
+        foreach ($instances as $instance) {
+            if (is_a($instance, 'WP_Post')) {
+                $subdomain = get_post_meta($instance->ID, 'subdomain',true);
+                $assoc_arr['subdomains']['sub'][$subdomain] = $instance->post_title;
+                if (false)
+                {
+                    shell_exec('bash setup.sh <'.$subdomain.'> <'.$instance->post_title.'>');
+
+                }
+            }
         }
+        $this->write_ini_file($assoc_arr,dirname(get_home_path()) . '/multi-moodle-instances/instances.ini', true);
+
+
 
     }
+
+
+    /**
+     * @param $assoc_arr
+     * @param $path
+     * @param $has_sections
+     * @return false|int
+     *
+     * written by Harikrishnan
+     * https://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
+     */
+    function write_ini_file($assoc_arr, $path, $has_sections = FALSE)
+    {
+        $content = "";
+        if ($has_sections) {
+            foreach ($assoc_arr as $key => $elem) {
+                $content .= "[" . $key . "]\n";
+                foreach ($elem as $key2 => $elem2) {
+                    if (is_array($elem2)) {
+                        foreach ($elem2 as $key3 => $elem3)
+                        {
+                            $content .= $key2 . "[$key3] = \"" . $elem3 . "\"\n" ;
+                        }
+                    } else if ($elem2 == "") $content .= $key2 . " = \n";
+                    else $content .= $key2 . " = \"" . $elem2 . "\"\n";
+                }
+            }
+        } else {
+            foreach ($assoc_arr as $key => $elem) {
+                if (is_array($elem)) {
+                    for ($i = 0; $i < count($elem); $i++) {
+                        $content .= $key . "[] = \"" . $elem[$i] . "\"\n";
+                    }
+                } else if ($elem == "") $content .= $key . " = \n";
+                else $content .= $key . " = \"" . $elem . "\"\n";
+            }
+        }
+
+        if (!$handle = fopen($path, 'w')) {
+            return false;
+        }
+
+        $success = fwrite($handle, $content);
+        fclose($handle);
+
+        return $success;
+    }
 }
+
 new ListInstances();
